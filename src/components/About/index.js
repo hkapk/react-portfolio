9381a53bcfd990fc271f06363d8d19ccddeee65e
Hank Kapka <hankkapka@gmail.com>
2022-02-06 12:00:03 -0600

    adding components

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h1 id="about">About me</h1>
+      <p>lorem</p>
+    </section>
+  );
+}
+
+export default About;

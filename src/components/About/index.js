e5ed3e993668020eedff9f7ec0f54ff131284bf8
Hank Kapka <hankkapka@gmail.com>
2022-02-06 19:33:12 -0600

    styled about me and contact me box

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,14 +5,18 @@ function About() {
   return (
     <section>
       <h1 id="about">About me</h1>
-      <div>
-      <img src={hankImg} className="my-2" style={{ width: "25%" }} alt="hank" />
-      <p className="my-2" style={{width: "50%"}}>
+      <div className="row">
+      <div className="picture">
+      <img src={hankImg} className="my-2" style={{ width: "90%" }} alt="hank" />
+      </div>
+      <div className="about">
+      <p style={{width: "55%"}}>
       Hi, I'm Hank and welcome to my portfolio! I am an aspiring web developer 
       with a passion for developing my skills. Lately I have been exploring mySQL, 
       sequelize, mongoDB and mongoose on the backend and React on the front end. You can check out my 
       projects in my portfolio, or on my Github, linked below.</p>
       <a href="https://www.github.com/hkapk">Check out my Github</a>
+      </div>
       </div>
     </section>
   );

7eb22b7503648a94d46aee043b6b94610a5d7d4c
Hank Kapka <hankkapka@gmail.com>
2025-02-04 12:42:00 -0600

    added to projects page

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,6 +6,9 @@ import zoo from "../../assets/project-img/zookeeper.png"
 import note from "../../assets/project-img/note.png";
 import food from "../../assets/project-img/food.png";
 import dgscore from "../../assets/project-img/dg.png";
+import dgskins from "../../assets/project-img/DgSkins.png";
+import bingo from "../../assets/project-img/WaldoBingo.png";
+
 
 
 function Portfolio() {
@@ -13,7 +16,9 @@ function Portfolio() {
         <section id="portfolio">
             <h1 id="about">Here are a few of my recent projects</h1>
             <p className="topic"> Clicking the icons will take you to their deployments.</p>
+
             <div className="row profile-row">
+
             <a href="https://calm-headland-49546.herokuapp.com/">
             <img src={budget} className="profile-icon" style={{ width: "1080px" }} alt="budget-tracker" />
             <a href="https://github.com/hkapk/budget-tracking-PWA">
@@ -21,7 +26,7 @@ function Portfolio() {
                 </a></a>
 
             <a href="https://calm-peak-91863.herokuapp.com/">
-            <img src={dgscore} className="profile-icon" style={{ width: "1080px" }} alt="budget-tracker" />
+            <img src={dgscore} className="profile-icon" style={{ width: "1080px" }} alt="score-keeper" />
             
             <a href="https://github.com/hkapk/dg-score-card">
                 <p className="profile-text">View on Github</p> 
@@ -33,7 +38,26 @@ function Portfolio() {
             <p className="profile-text">View on Github</p>
              </a> </a>
              </div>
-             
+
+
+             <div className="row profile-row">
+            <a href="https://waldo-caller.vercel.app/">
+            <img src={bingo} className="profile-icon" style={{ width: "1080px" }} alt="waldo-bingo" />
+            <a href="https://github.com/hkapk/waldo-caller">
+
+            <p className="profile-text">View on Github</p></a>  </a>
+
+            <a href="https://hkapk.github.io/Disc-Golf-Skins-ScoreKeeper/">
+            <img src={dgskins} className="profile-icon" style={{ width: "1080px" }} alt="discgolf-skins" />
+            
+            <a href="https://github.com/hkapk/Disc-Golf-Skins-ScoreKeeper">
+            <p className="profile-text">View on Github</p>
+            </a> </a>
+            </div>
+
+            
+
+
              <div className="row profile-row">
             <a href="https://trippin-worldwide.herokuapp.com/">
             <img src={trippin} className="profile-icon" style={{ width: "1080px" }} alt="trippin-worldwide" />
@@ -41,6 +65,7 @@ function Portfolio() {
             <a href="https://github.com/hkapk/trippin-worldwide"> 
             <p className="profile-text">View on Github</p></a></a>
             </div>
+
             <div className="row profile-row">
             <a href="https://calm-sea-20605.herokuapp.com/">
             <img src={zoo} className="profile-icon" style={{ width: "1080px" }} alt="zoo-keeper" />
@@ -54,6 +79,7 @@ function Portfolio() {
             <p className="profile-text">View on Github</p>
             </a> </a>
             </div>
+
             <div className="row profile-row">
             <a href="https://hkapk.github.io/food-festival/index.html">
             <img src={food} className="profile-icon" style={{ width: "1080px" }} alt="note-taker" />

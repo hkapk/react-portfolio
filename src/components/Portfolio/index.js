6c52d8fdfbfc418c63e673e20bc50c47d634f58c
Hank Kapka <hankkapka@gmail.com>
2025-02-07 08:41:47 -0600

    updated portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -16,7 +16,21 @@ function Portfolio() {
         <section id="portfolio">
             <h1 id="about">Here are a few of my recent projects</h1>
             <p className="topic"> Clicking the icons will take you to their deployments.</p>
+            
+            <div className="row profile-row">
+            <a href="https://waldo-caller.vercel.app/">
+            <img src={bingo} className="profile-icon" style={{ width: "1080px" }} alt="waldo-bingo" />
+            <a href="https://github.com/hkapk/waldo-caller">
 
+            <p className="profile-text">View on Github</p></a>  </a>
+
+            <a href="https://hkapk.github.io/Disc-Golf-Skins-ScoreKeeper/">
+            <img src={dgskins} className="profile-icon" style={{ width: "1080px" }} alt="discgolf-skins" />
+            
+            <a href="https://github.com/hkapk/Disc-Golf-Skins-ScoreKeeper">
+            <p className="profile-text">View on Github</p>
+            </a> </a>
+            </div>
             <div className="row profile-row">
 
             <a href="https://calm-headland-49546.herokuapp.com/">
@@ -39,23 +53,6 @@ function Portfolio() {
              </a> </a>
              </div>
 
-
-             <div className="row profile-row">
-            <a href="https://waldo-caller.vercel.app/">
-            <img src={bingo} className="profile-icon" style={{ width: "1080px" }} alt="waldo-bingo" />
-            <a href="https://github.com/hkapk/waldo-caller">
-
-            <p className="profile-text">View on Github</p></a>  </a>
-
-            <a href="https://hkapk.github.io/Disc-Golf-Skins-ScoreKeeper/">
-            <img src={dgskins} className="profile-icon" style={{ width: "1080px" }} alt="discgolf-skins" />
-            
-            <a href="https://github.com/hkapk/Disc-Golf-Skins-ScoreKeeper">
-            <p className="profile-text">View on Github</p>
-            </a> </a>
-            </div>
-
-            
 
 
              <div className="row profile-row">
